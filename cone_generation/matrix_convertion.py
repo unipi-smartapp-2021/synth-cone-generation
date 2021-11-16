2058cc8275736771ff49c96afbeee955102e33a6
Federico Silvestri <federico.silvestri@vedrai.com>
2021-11-16 16:21:37 +0100

    Improve function readability and usage

--- cone_generation/matrix_convertion.py
+++ cone_generation/matrix_convertion.py
@@ -5,36 +5,33 @@ from random import choices
 
 
 def convert_image_to_matrix(filepath: str):
+    """
+    Convert an image to grayscale and then to a numpy matrix.
+
+    :param filepath: filepath of image
+    :return: the matrix
+    """
     with Image.open(filepath) as image:
-        # Convert the image to GrayScale, so we convert a tensor into a simple matrix
+        # Convert the image to GrayScale, so we convert a tensor into a simple 2D matrix
         gray_scale_image = image.convert('L')
+
         # Convert to a numpy array
         np_arr = np.asarray(gray_scale_image)
 
         (width, height) = np_arr.shape
 
-        print(np.unique(np_arr))
-
+        # with this two cycle we set to 255 the
+        # pixel where the cone is placed.
         for i in range(0, width):
             for j in range(0, height):
                 pixel = np_arr[i][j]
                 if pixel == 255:
                     pixel = 0  # this is the background
                 else:
-                    pixel = choices(
-                        population=[255, 0],
-                        weights=[0.1, 0.9],
-                        k=1
-                    )
-
-                    pixel = pixel[0]
-
+                    pixel = 255
                 # set the pixel
                 np_arr[i][j] = pixel
 
-        plt.imshow(np_arr)
-        plt.show()
-
-
-convert_image_to_matrix('./tmp/Figure_1.png')
+        # return the matrix
+        return np_arr
 
